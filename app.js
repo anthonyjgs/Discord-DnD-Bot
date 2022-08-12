6cd9344c4c6d154765e8f7df2c1797142ae691d3
anthonyjgs <86584710+anthonyjgs@users.noreply.github.com>
2022-08-12 12:18:25 -0700

    Installed fundemental modules and started setting up the main app.js basics. Bot can now start up and login to a server.

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-Discord = 
+// Setup access to discord.js module via Discord const
+const Discord = require("discord.js");
+// Create bot client and its intents
+const client = new Discord.Client({intents: ["GuildMessages"]});
+
+// Setup the environment variables
+/* NOTE: the .env variables do not autocomplete, but they are still accessed
+    via process.env.VARIABLE_NAME*/
+require('dotenv').config()
+const botToken = process.env.DISCORD_TOKEN;
+
+// Runs when the bot finishes starting up
+client.once("ready", () => {
+    console.log("DnD bot is online!");
+})
+
+// Required to login as the bot
+client.login(botToken);
